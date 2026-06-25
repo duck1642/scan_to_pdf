6f795193301193125f6eb5f669c62908e8001ea3
duck1642 <keremefeozcelik@hotmail.com>
2026-06-25 19:40:59 +0300

    Auto-open browser to http://localhost:5173 in run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
 start "" cmd /k "cd /d %~dp0backend && venv\Scripts\python app.py"
 start "" cmd /k "cd /d %~dp0frontend && npm run dev"
+
+:: Wait 2 seconds for Vite to initialize, then automatically open the UI in your default browser
+timeout /t 2 >nul
+start http://localhost:5173
